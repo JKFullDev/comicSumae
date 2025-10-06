4e309927346e7a47af297a1d05d151d943b9a200
JKFullDev <jcah009@gmail.com>
2025-10-07 00:23:27 +0200

    First commit

---
+++ home.js
@@ -0,0 +1,30 @@
+const barraBusqueda = document.querySelector("#buscar");
+const main = document.querySelector("main")
+
+barraBusqueda.addEventListener("click", () => {
+    const cuadroBusqueda = document.createElement("div");
+    cuadroBusqueda.id = "cuadroBusqueda";
+    cuadroBusqueda.innerHTML = `
+    <div>
+        <input id="barraBusqueda" type="text" placeholder="Busca">
+        <button onclick="location.href = './home.html'">X</button>
+    </div>
+    <ul>
+        <li>
+            <img src="https://icons.veryicon.com/png/o/miscellaneous/rookie-official-icon-gallery/225-default-avatar.png" alt="avatar" onclick="location.href = './avatarBiblioteca.html'">
+            <h3>User 1</h3>
+        </li>
+        <li>
+            <img src="https://icons.veryicon.com/png/o/miscellaneous/rookie-official-icon-gallery/225-default-avatar.png" alt="avatar">
+            <h3>User 2</h3>
+        </li>
+        <li>
+            <img src="https://icons.veryicon.com/png/o/miscellaneous/rookie-official-icon-gallery/225-default-avatar.png" alt="avatar">
+            <h3>User 3</h3>
+        </li>
+    </ul>
+    `;
+
+    main.innerHTML = ``;
+    main.appendChild(cuadroBusqueda)
+});
---
+++ main.js
@@ -0,0 +1,101 @@
+const API = "http://localhost:3000/comics";
+const lista = document.querySelector("#comics-container");
+const formulario = document.querySelector("#form-comic");
+
+let modoEdicion = false;
+
+
+const crearComic = (comic) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <h2>${comic.editorial} - ${comic.titulo}</h2>
+        <img src="${comic.cover}" alt="${comic.editorial} - ${comic.titulo}">
+        <p>${comic.year}</p>
+    `;
+
+    //Añadimos el botón y la función de borrar tanto de la web como de la API
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Borrar comic";
+    li.appendChild(deleteBtn);
+    deleteBtn.addEventListener("click", async () => {
+        await fetch(`${API}/${comic.id}`, {
+            method: "DELETE"
+        });
+
+        consultarComics();
+    });
+    //Creamos boton de editar
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Editar comic";
+    li.appendChild(editBtn);
+    editBtn.addEventListener("click", () => {
+        modoEdicion = true;
+        document.querySelector("#submitBtn").textContent = "Editar comic";
+        //Pintamos los datos del comic existente en el formulario
+        document.querySelector("#titulo").value = comic.titulo;
+        document.querySelector("#editoriala").value = comic.editorial;
+        document.querySelector("#portada").value = comic.cover;
+        document.querySelector("#año").value = comic.year;
+        document.querySelector("#id").value = comic.id;
+    });
+    return li;
+}
+
+//GET
+const consultarComics = async () => {
+    //COlocamos un loading
+    lista.innerHTML = "Cargando...";
+    //Atacamos a nuestra API
+    const res = await fetch(API);
+    const data = await res.json();
+    //Vaciamos la lista
+    lista.innerHTML = "";
+    data.forEach((comic) => lista.appendChild(crearComic(comic)));
+}
+
+//POST
+formulario.addEventListener("submit", async (ev) => {
+    ev.preventDefault();
+
+    if (!modoEdicion) {
+        //Creamos el nuevo objeto comic
+        const crearComic = {
+            "id": crypto.randomUUID(),
+            "titulo": document.querySelector("#titulo").value,
+            "editorial": document.querySelector("#editoriala").value,
+            "year": document.querySelector("#año").value,
+            "cover": document.querySelector("#portada").value,
+        };
+        //Lo maandamos a la base de datos mediante fetch
+        await fetch(API, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(crearComic)
+        });
+    } else {
+        //Recuperamos el id
+        const inputID = document.querySelector("#id").value;;
+        //Editamos el comic seleccionado
+        const crearComic = {
+            "id": document.querySelector("#id").value,
+            "titulo": document.querySelector("#titulo").value,
+            "editorial": document.querySelector("#editoriala").value,
+            "year": document.querySelector("#año").value,
+            "cover": document.querySelector("#portada").value,
+        };
+        await fetch(`${API}/${inputID}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(crearComic),
+        });
+        modoEdicion = false;
+    }
+    //Reiniciamos el formulario
+    formulario.reset();
+    //Repintamos los Comics para poder ver los cambios a tiempo real
+    await consultarComics();
+});
+
+
+
+document.addEventListener("DOMContentLoaded", consultarComics);
